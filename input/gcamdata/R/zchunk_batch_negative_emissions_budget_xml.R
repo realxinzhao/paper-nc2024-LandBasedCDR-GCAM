03063edb60841c0bb78de23e0fabc88b757668f0
Zhao, Xin <xin.zhao@pnnl.gov>
2022-11-01 14:59:33 -0400

    Add a min price tag in negative emission budget

--- input/gcamdata/R/zchunk_batch_negative_emissions_budget_xml.R
+++ input/gcamdata/R/zchunk_batch_negative_emissions_budget_xml.R
@@ -46,6 +46,9 @@ module_energy_batch_negative_emissions_budget_xml <- function(command, ...) {
         add_xml_data(L270.CTaxInput, "GlobalTechCTaxInput") %>%
         add_xml_data(L270.LandRootNegEmissMkt, "LandRootNegEmissMkt") %>%
         add_xml_data(L270.NegEmissBudgetMaxPrice, "PortfolioStdMaxPrice") %>%
+        # testing a min policy price here
+        add_xml_data(L270.NegEmissBudgetMaxPrice %>% mutate(min.price = 0),
+                     "PortfolioStdMinPrice") %>%
         add_xml_data(get_data(all_data, curr_data_name), "PortfolioStd") %>%
         add_precursors("L270.CTaxInput", "L270.LandRootNegEmissMkt", "L270.NegEmissBudgetMaxPrice", curr_data_name) %>%
         assign(curr_xml_name, ., envir = curr_env)
